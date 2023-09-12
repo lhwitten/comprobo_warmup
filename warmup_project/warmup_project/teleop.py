f7d6c7c1e4ecf3847d847eefc7df6f365f849c90
Cole Marco <cmarcoedu@gmail.com>
2023-09-12 13:13:06 -0400

    Refactor teleop

--- warmup_project/warmup_project/teleop.py
+++ warmup_project/warmup_project/teleop.py
@@ -1,3 +1,9 @@
+"""
+Used online repository at
+https://github.com/rohbotics/ros2_teleop_keyboard/blob/master/teleop_twist_keyboard.py
+to understand how teleop works and get scaffold
+"""
+
 #!/usr/bin/env python3
 import rclpy
 import tty
@@ -8,6 +14,32 @@ from geometry_msgs.msg import Twist
 
 settings = termios.tcgetattr(sys.stdin)
 key = None
+
+intro_msg = """
+    Reading from the keyboard  and Publishing to Twist!
+    ---------------------------
+    Moving around:
+    u    i    o
+    j    k    l
+    m    ,    .
+
+    For Holonomic mode (strafing), hold down the shift key:
+    ---------------------------
+    U    I    O
+    J    K    L
+    M    <    >
+
+    t : up (+z)
+    b : down (-z)
+
+    anything else : stop
+
+    q/z : increase/decrease max speeds by 10%
+    w/x : increase/decrease only linear speed by 10%
+    e/c : increase/decrease only angular speed by 10%
+
+    CTRL-C to quit
+    """
 
 moveBindings = {
 		'i':(1,0,0,0),
@@ -50,31 +82,7 @@ def getKey():
 def main(args=None):
     rclpy.init(args=args)
 
-    print("""
-    Reading from the keyboard  and Publishing to Twist!
-    ---------------------------
-    Moving around:
-    u    i    o
-    j    k    l
-    m    ,    .
-
-    For Holonomic mode (strafing), hold down the shift key:
-    ---------------------------
-    U    I    O
-    J    K    L
-    M    <    >
-
-    t : up (+z)
-    b : down (-z)
-
-    anything else : stop
-
-    q/z : increase/decrease max speeds by 10%
-    w/x : increase/decrease only linear speed by 10%
-    e/c : increase/decrease only angular speed by 10%
-
-    CTRL-C to quit
-    """)
+    print(intro_msg)
 
     # setup node and publisher for transmitting telemetry
     node = rclpy.create_node('teleop')		
